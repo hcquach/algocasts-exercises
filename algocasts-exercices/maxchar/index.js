be2ca4a93f2148b12d00ff9587abf4808a651677
Hien-Chanh Quach <hcquach@gmail.com>
2018-08-08 15:52:56 +0200

    Solved MaxChar

--- algocasts-exercices/maxchar/index.js
+++ algocasts-exercices/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {};
+  let max = "";
+  for (let char of str) {
+    chars[char] = chars[char] + 1 || 1;
+  }
+  chars[max] = 0;
+  for (let key in chars) {
+    if (chars[key] > chars[max]) {
+      max = key;
+    }
+  }
+  return max;
+}
 
 module.exports = maxChar;
