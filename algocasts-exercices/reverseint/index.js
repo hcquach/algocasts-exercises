610430db5a83fde4bfe15993c7e01768f6b90382
Hien-Chanh Quach <hcquach@gmail.com>
2018-08-08 11:29:03 +0200

    Solved ReverseInt

--- algocasts-exercices/reverseint/index.js
+++ algocasts-exercices/reverseint/index.js
@@ -8,6 +8,14 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+
+  // HC's solution, reversing the digits, then adding the minus if negative
+  // const reversed_digits = parseInt(n.toString().split('').reverse().join(''), 10);
+  // return (n < 0 ? -1 * reversed_digits : reversed_digits);
+
+  // Trial with Math.sign returning -1 if negative directly
+  return Math.sign(n) * parseInt(n.toString().split('').reverse().join(''), 10);
+}
 
 module.exports = reverseInt;
