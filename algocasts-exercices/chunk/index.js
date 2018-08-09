dcfe830c4a8d27a507854df6d26363f85576f6d9
Hien-Chanh Quach <hcquach@gmail.com>
2018-08-09 11:44:19 +0200

    Chunk exercise

--- algocasts-exercices/chunk/index.js
+++ algocasts-exercices/chunk/index.js
@@ -8,6 +8,14 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let chunkArray = [];
+  let i = 0;
+  while (i < array.length) {
+    chunkArray.push(array.slice(i, i + size));
+    i += size;
+  }
+  return chunkArray;
+}
 
 module.exports = chunk;
