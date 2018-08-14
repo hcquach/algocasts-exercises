b438b20af32b27e47e04bb1a76327f0596a34c62
Hien-Chanh Quach <hcquach@gmail.com>
2018-08-14 09:34:39 +0200

    Solved Vowels

--- algocasts-exercices/vowels/index.js
+++ algocasts-exercices/vowels/index.js
@@ -7,6 +7,15 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const vowel = ["a", "e", "i", "o", "u"];
+  let vowelCount = 0;
+  for (let char of str.toLowerCase()) {
+    if (vowel.includes(char)) {
+      vowelCount += 1;
+    }
+  }
+  return vowelCount;
+}
 
 module.exports = vowels;
