0e1e6e607e481d897d209aa547e0527a755c6c22
Hien-Chanh Quach <hcquach@gmail.com>
2018-08-23 10:26:47 +0200

    Binary Search Tree

--- algocasts-exercices/bst/index.js
+++ algocasts-exercices/bst/index.js
@@ -11,6 +11,66 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    // Answer without recursion
+    // let check = this;
+    // const newNode = new Node(data);
+    // while (check) {
+    //   if (data > check.data) {
+    //     if (check.right) {
+    //       check = check.right;
+    //     }
+    //     else {
+    //       check.right = newNode;
+    //       return;
+    //     }
+    //   }
+    //   else {
+    //     if (check.left) {
+    //       check = check.left;
+    //     }
+    //     else {
+    //       check.left = newNode;
+    //       return;
+    //     }
+    //   }
+    // }
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+    }
+    else if (data < this.data) {
+      this.left = new Node(data);
+    }
+    else if (data > this.data && this.right) {
+      this.right.insert(data);
+    }
+    else if (data > this.data) {
+      this.right = new Node(data);
+    }
+  }
+
+  contains(data) {
+     if (data < this.data && this.left) {
+      return this.left.contains(data);
+    }
+    else if (data > this.data && this.right) {
+      return this.right.contains(data);
+    }
+    else if (data === this.data) {
+      return this;
+    }
+    else {
+      return null;
+    }
+  }
+
+}
 
 module.exports = Node;
